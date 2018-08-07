2ab813228ebfee0164abd8bacdff3e6cce77b724
Suzanne Richman <suzanne@favoredfortune.com>
2018-08-07 11:10:27 -0700

    added structure for story engine for Amy to work on and move controller methods to

--- src/main/java/com/questing/littlequest/engine/StoryEngine.java
+++ src/main/java/com/questing/littlequest/engine/StoryEngine.java
@@ -1,4 +1,26 @@
-package com.questing.littlequest.engine;
-
-public interface StoryEngine {
-}
+//package com.questing.littlequest.engine;
+//
+//import com.questing.littlequest.models.Stories;
+//
+//import java.util.List;
+//
+//public class StoryEngine {
+//    public StoryEngine(){
+//         Long story_id;
+//         String title;
+//         Long root_choice;
+//         Long user_id;
+//    }
+//    public Stories getContentText(){
+//
+//    }
+//    public List<Stories> getCurrentChoices(){
+//
+//    }
+//    public boolean isGameOver(){
+//
+//    }
+//    public void doChoice(parameters){
+//
+//    }
+//}
