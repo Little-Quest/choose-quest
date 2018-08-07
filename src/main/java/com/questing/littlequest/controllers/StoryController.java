128b56e2870af80e42b70b9666465e24aea7c499
Amy Cohen <anbcohen@gmail.com>
2018-08-07 10:56:15 -0700

    saving changes to StoryController

--- src/main/java/com/questing/littlequest/controllers/StoryController.java
+++ src/main/java/com/questing/littlequest/controllers/StoryController.java
@@ -1,7 +1,28 @@
 package com.questing.littlequest.controllers;
 
+import com.questing.littlequest.models.Stories;
+import com.questing.littlequest.repositories.ChoicesRepository;
+import com.questing.littlequest.repositories.StoriesRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
+@Controller
 public class StoryController {
 
+    @Autowired
+    StoriesRepository storiesRepository;
 
+    @Autowired
+    ChoicesRepository choicesRepository;
+
+    @GetMapping("/story-choice")
+    public String storyDisplay(Model model) {
+        List<Stories> stories = storiesRepository.findAll();
+        model.addAttribute("stories", stories);
+        return "redirect:/story-choice";
+    }
 }
