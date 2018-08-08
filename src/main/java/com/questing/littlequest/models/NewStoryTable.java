eb9cc37ea6c91092f3432bf56c70197e5dcda139
Amy Cohen <anbcohen@gmail.com>
2018-08-07 17:13:20 -0700

    added a new story table model and repository

---
+++ src/main/java/com/questing/littlequest/models/NewStoryTable.java
@@ -0,0 +1,28 @@
+package com.questing.littlequest.models;
+
+import javax.persistence.*;
+
+    @Entity
+    @Table(name = "storytable-new")
+public class NewStoryTable {
+    @Id
+    @GeneratedValue
+    @SequenceGenerator(name="prompt-id-generator")
+
+    public Long prompt_id;
+    public Long story_id;
+    public String title;
+
+    public NewStoryTable() {
+    }
+
+    public NewStoryTable(Long story_id, String title) {
+        this.title = title;
+        this.story_id = story_id;
+    }
+
+    public String toString() {
+        return this.title;
+    }
+}
+
---
+++ src/main/java/com/questing/littlequest/repositories/NewStoryTableRepository.java
@@ -0,0 +1,6 @@
+package com.questing.littlequest.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NewStoryTableRepository extends JpaRepository<NewStoryTableRepository, Long> {
+}
