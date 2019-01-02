5a2069f9ed5a9c77cf64850ad2f5cc2cf59a8bfc
Suzanne Richman <suzanne@favoredfortune.com>
2019-01-02 12:18:37 -0800

    updated common thymeleaf to hold fragments for nav and footer; corrected minor errors in buttons/routing for logout/delete user in nav/footer; changed about-us page to be template to use thymleaf fragment; updated about-us page for image routing; updated logo to link to index page; created about controller

---
+++ src/main/java/com/questing/littlequest/controllers/AboutController.java
@@ -0,0 +1,21 @@
+package com.questing.littlequest.controllers;
+
+//external imports from Springboot for MVC
+import com.questing.littlequest.models.Stories;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+//for delivering content pages about the application itself
+@Controller
+public class AboutController {
+    @GetMapping("/about-us")
+    public String about(Model model) {
+        model.addAttribute("about-us", model);
+        return "about-us";
+    }
+}
